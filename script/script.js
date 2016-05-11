618adf618116b57dbaa177360d4c3a7144c45bb3
joshuaryaan <joshua.ryan.warner@icloud.com>
2016-05-11 15:13:54 +1200

    Initial commit
    A quick sketch to test on my newly acquired jQuery skills in early 2015.

---
+++ script/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+jQuery("#is").click(function() {
+    jQuery(this).find('.is').toggle();
+});
+jQuery("#there").click(function() {
+    jQuery(this).find('.there').toggle();
+});
+jQuery("#life").click(function() {
+    jQuery(this).find('.life').toggle();
+});
+jQuery("#on").click(function() {
+    jQuery(this).find('.on').toggle();
+});
+jQuery("#mars").click(function() {
+    jQuery(this).find('.mars').toggle();
+});
+
+});
+
+
+
+
+
+//$( "#Layer_1 .path1" ).click(function() {
+//  $( "#Layer_1 .path1" ).toggle( //"fast", function() {
+    // Animation complete.
+//  });
+//});
